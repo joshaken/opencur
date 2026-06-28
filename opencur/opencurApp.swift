6a186844719594247704318c28d1be7f355b433f
zjq <jqchu96@outlook.com>
2026-06-28 21:29:34 +0800

    Initial Commit

---
+++ opencur/opencurApp.swift
@@ -0,0 +1,17 @@
+//
+//  opencurApp.swift
+//  opencur
+//
+//  Created by neo on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct opencurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
